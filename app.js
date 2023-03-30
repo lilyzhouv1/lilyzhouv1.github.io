7d997c5f5383d9b59c7489cc997426682a0bad1e
Lily Zhou <73075050+lilyzhou2002@users.noreply.github.com>
2023-03-30 01:52:24 -0400

    changed everything

--- app.js
+++ app.js
@@ -1,52 +1,146 @@
-var enterMoney = document.getElementById('enter_money')
+//Grab the income section
+var monthlyPay = document.getElementById('monthly_pay');
+var monthlyOther = document.getElementById('monthly_other');
+var annualTax = document.getElementById('annual_tax');
+
+//Grab the monthly expenses section
+var monthlyFood = document.getElementById('monthly_food');
+var monthlyClothes = document.getElementById('monthly_clothes');
+var monthlyRent = document.getElementById('monthly_rent');
+var monthlyUtilities = document.getElementById('monthly_utilities');
+var monthlyTransport = document.getElementById('monthly_transportation');
+var monthlyHealth = document.getElementById('monthly_health');
+var monthlyPersonal = document.getElementById('monthly_personal');
+var monthlyMisc = document.getElementById('monthly_miscellaneous');
+
+//Grab the annual expenses section
+var annualTuition = document.getElementById('annual_tuition');
+var annualCar = document.getElementById('annual_car');
+var annualVacation = document.getElementById('annual_vacation');
+var annualOther = document.getElementById('annual_other');
+
+//Grab the monthly savings section
+var savingEmergency = document.getElementById('saving_emergency');
+var savingInvestments = document.getElementById('saving_investments');
+var savingRRSP = document.getElementById('saving_rrsp');
+
+
+// results section
+var evaluate2 = document.getElementById('evaluate2');
+var results = document.getElementById('results');
+
 
 var money;
 
 //Grab the error message
 var error = document.getElementById('error');
-var errorManage = document.getElementById('error_manage');
 
-//get all manage inputs like (roomrent, accessories, emergency, and saving)
-var roomrent = document.getElementById('RoomRent');
-var accessories = document.getElementById('Accessories');
-var emergency = document.getElementById('Emergency');
-var saving = document.getElementById('Saving');
+//get output
+var showIncome = document.getElementById('show_income');
+var showMonthly = document.getElementById('show_Mexpenses');
+var showAnnual = document.getElementById('show_Aexpenses');
+var showSavings = document.getElementById('show_savings');
+var showBudget = document.getElementById('show_budget');
 
-//get all output element
-var showRoom = document.getElementById('show_room');
-var showAccessories = document.getElementById('show_access');
-var showEmergency = document.getElementById('show_emergency');
-var showSaving = document.getElementById('show_save');
 
 // get loader gif fcile
 var loader = document.getElementById('loader');
 
-//Grab the manage division section
-var manageDiv = document.getElementById('manage_div');
-
-//get the evaluate button
-var evaluate = document.getElementById('evaluate');
-
 //get reset section
 var resetButton = document.getElementById('reset_button');
 
-//get result section
-var resultSection = document.getElementById('result_section');
 
 
 //*Events*
 //Create an event that the input entered will be sent to the function showManageMoney when the user releases a key
-enterMoney.addEventListener('keyup', showManageMoney); //sent to an element when the user releases a key on the keyboard
-evaluate.addEventListener('click', showloader); //sent to the function validate when the button is clicked
+
+//Income Section
+monthlyPay.addEventListener('keyup', showManageMoney); //sent to an element when the user releases a key on the keyboard
+monthlyOther.addEventListener('keyup', showManageMoney);
+annualTax.addEventListener('keyup', showManageMoney);
+
+//Monthly Expenses section
+monthlyFood.addEventListener('keyup', showManageMoney);
+monthlyClothes.addEventListener('keyup', showManageMoney);
+monthlyRent.addEventListener('keyup', showManageMoney);
+monthlyUtilities.addEventListener('keyup', showManageMoney);
+monthlyTransport.addEventListener('keyup', showManageMoney);
+monthlyHealth.addEventListener('keyup', showManageMoney);
+monthlyPersonal.addEventListener('keyup', showManageMoney);
+monthlyMisc.addEventListener('keyup', showManageMoney);
+
+//Annual Expenses section
+annualTuition.addEventListener('keyup', showManageMoney);
+annualCar.addEventListener('keyup', showManageMoney);
+annualVacation.addEventListener('keyup', showManageMoney);
+annualOther.addEventListener('keyup', showManageMoney);
+
+//Monthly Savings section
+savingEmergency.addEventListener('keyup', showManageMoney);
+savingInvestments.addEventListener('keyup', showManageMoney);
+savingRRSP.addEventListener('keyup', showManageMoney);
+
+
+evaluate2.addEventListener('click', showloader2); //sent to the function validate when the button is clicked
+
 resetButton.addEventListener('click', reload);
 
 //function to show gif loader image
-function showloader(){
+function showloader2(){
     loader.classList.remove("hidden");
-    setTimeout(validateManage, 1000);
+    setTimeout(yourBudgetReview, 1000);
 }
 
-//function to validate input amount and show the manage section
+function yourBudgetReview(){
+    //hides the loading button
+    loader.classList.add("hidden");
+
+    results.classList.remove('hidden');
+
+    //income
+    var income1 = parseInt(monthlyPay.value);
+    var income2 = parseInt(monthlyOther.value);
+    var income3 = parseInt(annualTax.value);
+
+    //monthly expenses
+    var monthly1 = parseInt(monthlyFood.value);
+    var monthly2 = parseInt(monthlyClothes.value);
+    var monthly3 = parseInt(monthlyRent.value);
+    var monthly4 = parseInt(monthlyUtilities.value);
+    var monthly5 = parseInt(monthlyTransport.value);
+    var monthly6 = parseInt(monthlyHealth.value);
+    var monthly7 = parseInt(monthlyPersonal.value);
+    var monthly8 = parseInt(monthlyMisc.value);
+
+    //annual expenses
+    var annual1 = parseInt(annualTuition.value);
+    var annual2 = parseInt(annualCar.value);
+    var annual3 = parseInt(annualVacation.value);
+    var annual4 = parseInt(annualOther.value);
+
+    //monthly savings
+    var save1 = parseInt(savingEmergency.value);
+    var save2 = parseInt(savingInvestments.value);
+    var save3 = parseInt(savingRRSP.value);
+
+    //totalling the results
+    totalIncome = income1 + income2 + (income3 / 12);
+    totalMonthly = monthly1 + monthly2 + monthly3 + monthly4 + monthly5 + monthly6 + monthly7 + monthly8;
+    totalAnnual = (annual1 + annual2 + annual3 + annual4) / 12;
+    totalSavings = save1 + save2 + save3;
+
+    //calculating total budget
+    totalBudget = totalIncome - totalMonthly - totalAnnual - totalSavings;
+
+    //showing the results
+    showIncome.innerHTML = "$" + totalIncome.toFixed(2);
+    showMonthly.innerHTML = "$" + totalMonthly.toFixed(2);
+    showAnnual.innerHTML = "$" + totalAnnual.toFixed(2);
+    showSavings.innerHTML = "$" + totalSavings.toFixed(2);
+    showBudget.innerHTML = "$" + totalBudget.toFixed(2);
+
+}
+
 
 function showManageMoney(e)
 {
@@ -54,65 +148,22 @@ function showManageMoney(e)
     if(e.keyCode==13){
         money = e.target.value; //Once enter key is hit, money will be whatever the value is inside the input.
 
-        //validate the input value
-        if(isNaN(money) || money==0){
+        //if it's not a number it will show as error
+        if(isNaN(money)){
             //display error message
             error.classList.remove('hidden');
+            evaluate2.classList.add('hidden');
+            results.classList.add('hidden');
         }
         else{
-            //since money is not 0 or invalid, then show the manage section div
-            manageDiv.classList.remove('hidden');
+
+            evaluate2.classList.remove('hidden');
             error.classList.add('hidden'); //make sure the error message is still hidden
 
         }
 
     }
 
-}
-
-// function to validate
-function validateManage(){
-    //hide loader image
-    loader.classList.add("hidden");
-
-    //vaidate input fields
-    if(roomrent.value =="" || accessories.value=="" || emergency.value=="" || saving.value==""){
-        errorManage.innerHTML="Value for input fields is not given. Please provide the value for all inputs"
-    }
-    else{
-        errorManage.innerHTML="";
-        //parse the value to integer
-        var room_per= parseInt(roomrent.value);
-        var access_per=parseInt(accessories.value);
-        var emer_per=parseInt(emergency.value);
-        var save_per=parseInt(saving.value);
-
-        var total = room_per + access_per + emer_per + save_per;
-
-        //Display erorr message if the total is greater than 100%
-        if(total > 100 || total < 0){
-            errorManage.innerHTML="The total percentage is exceeding 100%. Please make sure that it doesn't exceed 100%"
-        }
-        else{
-            //validation is complete now calculate the percentage using the function below
-            calculate(room_per, access_per, emer_per, save_per);
-        }
-    }
-}
-
-//calculate the percentage
-function calculate(roomrent, accessories, emergency, saving){
-    var roomrentMoney = ( roomrent/100) * money;
-    var accessoriesMoney = ( accessories/100) * money;
-    var emergencyMoney = ( emergency/100) * money;
-    var savingMoney = ( saving/100) * money;
-
-    showRoom.innerHTML="$"+ roomrentMoney;
-    showAccessories.innerHTML = "$"+ accessoriesMoney;
-    showEmergency.innerHTML = "$" + emergencyMoney;
-    showSaving.innerHTML = "$" + savingMoney;
-
-    resultSection.classList.remove("hidden");
 }
 
 //reload the page
